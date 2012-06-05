c6e96c60bcac9823b6cbd281a1e1813cf26f8328
David Twist <dtwist@jadeorchard.com>
2012-06-04 17:01:00 -0700

    fix infinite loop bug, misc cleanup

--- jscripts/tiny_mce/plugins/pwllink/editor_plugin_src.js
+++ jscripts/tiny_mce/plugins/pwllink/editor_plugin_src.js
@@ -24,14 +24,6 @@
 
 				App.toolbar._actionLinkEdit(t);
 				
-				//ed.windowManager.open({
-				//	file : url + '/link.htm',
-				//	width : 480 + parseInt(ed.getLang('advlink.delta_width', 0)),
-				//	height : 400 + parseInt(ed.getLang('advlink.delta_height', 0)),
-				//	inline : 1
-				//}, {
-				//	plugin_url : url
-				//});
 			});
 
 			// Register buttons
@@ -56,32 +48,33 @@
 			},attributes);
 			var editor = this.editor;
 			var elm, elementArray, i;
-		
+			
 			elm = editor.selection.getNode();
-			checkPrefix(document.forms[0].href); //##########################
-		
+			
 			elm = editor.dom.getParent(elm, "A");
-		
+			
 			// Remove element if there is no href
-			if (!o.href) {
+			if (!attribs.href) {
 				i = editor.selection.getBookmark();
 				editor.dom.remove(elm, 1);
 				editor.selection.moveToBookmark(i);
-				this.execCommand("mceEndUndoLevel");
+				this.execCommandWithSugarOnTop("mceEndUndoLevel");
 				return;
 			}
-		
+			
 			// Create new anchor elements
 			if (elm === null) {
 				editor.getDoc().execCommand("unlink", false, null);
-				this.execCommand("mceInsertLink", false, "#mce_temp_url#", {skip_undo : 1});
-		
+				this.execCommandWithSugarOnTop("mceInsertLink", false, "#mce_temp_url#", {skip_undo : 1}); // ################# THIS IS FEEZING FIREFOX ##################
+				
 				elementArray = tinymce.grep(editor.dom.select("a"), function(n) {return editor.dom.getAttrib(n, 'href') == '#mce_temp_url#';});
-				for (i=0; i<elementArray.length; i++)
-					setAllAttribs(elm = elementArray[i], attr);
-			} else
-				setAllAttribs(elm, attr);
-		
+				for (i=0; i<elementArray.length; i++) {
+					this.setAllAttribs(elm = elementArray[i], attribs);
+				}
+			} else {
+				this.setAllAttribs(elm, attribs);
+			}
+			
 			// Don't move caret if selection was image
 			if (elm.childNodes.length != 1 || elm.firstChild.nodeName != 'IMG') {
 				editor.focus();
@@ -89,11 +82,11 @@
 				editor.selection.collapse(0);
 				this.storeSelection();
 			}
-		
-			this.execCommand("mceEndUndoLevel");
+			
+			this.execCommandWithSugarOnTop("mceEndUndoLevel");
 		},
 		
-		setAllAttribs : function(elm, attribs){
+		setAllAttribs : function(elm, attr){
 			if (attr.href) {
 				this.editor.dom.setAttrib(elm, 'href', attr.href);
 			}
@@ -105,8 +98,8 @@
 			}
 		},
 
-		// cribbed from popup.js
-		execCommand : function(cmd, ui, val, a) {
+		// cribbed from popup.js and renamed to prevent infinite loop in main execCommand function
+		execCommandWithSugarOnTop : function(cmd, ui, val, a) {
 			a = a || {};
 			a.skip_focus = 1;
 	
@@ -115,14 +108,13 @@
 		},
 		// cribbed from popup.js
 		storeSelection : function() {
-			this.editor.windowManager.bookmark = tinyMCEPopup.editor.selection.getBookmark(1);
+			this.editor.windowManager.bookmark = this.editor.selection.getBookmark(1);
 		},
 		// cribbed from popup.js
 		restoreSelection : function() {
-			var t = this;
-	
-			if (!t.isWindow && tinymce.isIE)
-				t.editor.selection.moveToBookmark(t.editor.windowManager.bookmark);
+			if (tinymce.isIE) {
+				this.editor.selection.moveToBookmark(this.editor.windowManager.bookmark);
+			}
 		},
 
 
