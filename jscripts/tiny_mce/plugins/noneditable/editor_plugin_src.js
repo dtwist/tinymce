69ff195d9cc7d89e885a998bf5aba3aae2cbd155
David Twist <dtwist@jadeorchard.com>
2012-07-15 15:43:33 -0700

    code cleanup

--- jscripts/tiny_mce/plugins/noneditable/editor_plugin_src.js
+++ jscripts/tiny_mce/plugins/noneditable/editor_plugin_src.js
@@ -350,7 +350,7 @@
 				return true;
 			}
 
-			startElement = selection.getStart()
+			startElement = selection.getStart();
 			endElement = selection.getEnd();
 
 			// Disable all key presses in contentEditable=false except delete or backspace
